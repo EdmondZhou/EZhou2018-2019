0ca8b729e312dc37dbde92e07f2b129bd369c352
Edmond Zhou <19zhoue90@stu.smuhsd.org>
2018-08-20 10:43:52 -0700

    First commit to github

---
+++ Hello World/src/Hello_World.java
@@ -0,0 +1,18 @@
+/**
+ * Prints message in console
+ * 
+ * @author Edmond Zhou
+ * @version 8/17/2018
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Edmond"); 
+        System.out.println("Hello World! From Edmond"); 
+        System.out.println("Hello World! From Edmond"); 
+        System.out.println("Hello World! From Edmond"); 
+        System.out.println("Hello World! From Edmond"); 
+        System.out.println("Hello World! From Edmond"); 
+    }
+}
