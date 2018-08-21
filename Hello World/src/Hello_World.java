889e12520a82355a17f60ced26d2c710592e0eef
APCS3rd <APCS3rd@MHS-Math2-29>
2018-08-21 09:56:04 -0700

    Commit

--- Hello World/src/Hello_World.java
+++ Hello World/src/Hello_World.java
@@ -8,11 +8,6 @@ public class Hello_World
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From Edmond"); 
-        System.out.println("Hello World! From Edmond"); 
-        System.out.println("Hello World! From Edmond"); 
-        System.out.println("Hello World! From Edmond"); 
-        System.out.println("Hello World! From Edmond"); 
-        System.out.println("Hello World! From Edmond"); 
+        System.out.println("Hello World! From Edmond:) " ); 
     }
 }
