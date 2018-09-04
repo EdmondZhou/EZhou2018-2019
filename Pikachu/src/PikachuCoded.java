45399a6b1b9e4a6e41164a305bbfeac2e58bde8f
APCS3rd <APCS3rd@MHS-Math2-29>
2018-09-04 09:55:42 -0700

    Drawing a pickachu

---
+++ Pikachu/src/PikachuCoded.java
@@ -0,0 +1,13 @@
+/* This will print out Pickachu 
+ * @author Edmond Zhou
+ * @version 8/31/2018
+ */
+public class PikachuCoded {
+	public static void main (String[] args)
+	{
+		System.out.println("Pikachu welecomes you to the world of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)");
+		System.out.println("\t\t\t  (o^.^)");
+		System.out.println("\t\t\tz(_(\")(\")");
+}
+}
